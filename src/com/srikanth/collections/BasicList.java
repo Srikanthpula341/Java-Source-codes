bd8aa54b550e2b8d56ffcb3286b5643befe8191b
Srikanthpula341 <pulasrikanth1999@gmail.com>
2024-05-01 06:24:55 +0530

    setUp

---
+++ src/com/srikanth/collections/BasicList.java
@@ -0,0 +1,2 @@
+package com.srikanth.collections;public class BasicList {
+}
---
+++ src/com/srikanth/collections/stringList.java
@@ -0,0 +1,9 @@
+package com.srikanth.collections;
+/**
+ * Copyright (c) 2024 Srikanth Pula. All rights reserved.
+ * @author Srikanth Puls
+ * Date: 01-05-2024
+ */
+
+public class stringList {
+}
