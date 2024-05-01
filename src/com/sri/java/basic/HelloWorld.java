b3057a47e8f29f2b5e4f0ee596091e0cc6243e7b
Srikanthpula341 <pulasrikanth1999@gmail.com>
2024-05-01 06:45:07 +0530

    added first Programme

---
+++ src/com/sri/java/basic/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.sri.java.basic;
+
+/**
+ * This program serves as the entry point for learning Java. 
+ * It's designed to demonstrate the basics of setting up a Java project and 
+ * executing a simple Java program. 
+ * The goal for Day 1 is to get familiar with the Java programming environment and
+ *  to successfully run a "Hello World" program.
+ *
+ * Copyright (c) 2024 Srikanth Pula. All rights reserved.
+ *
+ * @author Srikanth Pula
+ * Date: 01-05-2024
+ */
+
+public class HelloWorld {
+    // The main method is the entry point of any Java application.
+    // For a "Hello World" program, it's the method that JVM invokes.
+    public static void main(String[] args) {
+        // Prints the string "This is my Hello World Program" to the standard output,
+        // commonly used to display text in a console or terminal.
+        System.out.println("This is my Hello World Program");
+    }
+}
